e5256f2e9d14769da95d30bdefc6d3dc36df1514
Mr-Ker <58399419+Mr-Ker@users.noreply.github.com>
2024-11-29 22:21:20 +0000

    Add clap argument parser
    The program will accept 2 optional arguments, one to the select the
    input folder, the other one to select for folder for the inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("input_examples"))]
+    input_folder: String,
+    #[arg(short, long, default_value_t = 1)]
+    day: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Running day {} with input folder <{}>!", args.day, args.input_folder);
 }
